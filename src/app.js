c8e6e9fd2dfff15d63175440ac7aa121a57aaf73
Satyadev Maurya <in.satyadev@gmail.com>
2024-05-24 23:37:29 +0530

    create routes and db connection

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const db = require('../src/db/connect')
+
+const app = express();
+const port = process.env.PORT || 3400;
+
+
+app.get("/", async (req, res) => {
+    res.send("Hello from the server")
+})
+
+app.listen(port, () =>{
+    console.log(`server running on http://localhost:${port}`)
+})
+
