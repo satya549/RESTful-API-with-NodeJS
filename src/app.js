7617a2896ce9341d7a6d75be656c5916ecc46f9c
Satyadev Maurya <in.satyadev@gmail.com>
2024-05-26 23:30:06 +0530

    create model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const db = require('../src/db/connect')
+const MenRanking = require("../src/models/mens");
+
 
 const app = express();
 const port = process.env.PORT || 3400;
