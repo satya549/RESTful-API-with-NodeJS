eeb464be1bcde6c179e580a0f476f854714b5364
Satyadev Maurya <in.satyadev@gmail.com>
2024-05-28 22:48:39 +0530

    Build post and get api

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const db = require('../src/db/connect')
 const MenRanking = require("../src/models/mens");
+const MensRanking = require('../src/models/mens');
 
 
 const app = express();
 const port = process.env.PORT || 3400;
 
+app.use(express.json());
 
-app.get("/", async (req, res) => {
-    res.send("Hello from the server")
+
+app.post("/mens", async (req, res) => {
+    try {
+        const MensRecords = new MensRanking(req.body)
+        const result = await MensRecords.save();
+        res.send(result)
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+app.get("/mens", async (req, res) => {
+    try {
+        const getMens = await MenRanking.find({})
+        res.send(getMens)
+    } catch (error) {
+        res.status(400).send(error);
+    }
 })
 
 app.listen(port, () =>{
